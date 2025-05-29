6330f5f062d99900d4f8b005557f17bfc5239847
Klesh Wong <klesh@qq.com>
2025-05-29 14:10:21 +0800

    docs: add metadata for zmk-studio

--- src/behaviors/behavior_mouse_setting.c
+++ src/behaviors/behavior_mouse_setting.c
@@ -14,6 +14,33 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata param_values[] = {
+    {
+        .display_name = "Sens+",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MS_TP_SENSITIVITY_INCR,
+    },
+    {
+        .display_name = "Sens-",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_VALUE,
+        .value = MS_TP_SENSITIVITY_DECR,
+    },
+};
+
+static const struct behavior_parameter_metadata_set param_metadata_set[] = {{
+    .param1_values = param_values,
+    .param1_values_len = ARRAY_SIZE(param_values),
+}};
+
+static const struct behavior_parameter_metadata metadata = {
+    .sets_len = ARRAY_SIZE(param_metadata_set),
+    .sets = param_metadata_set,
+};
+
+#endif
+
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     switch (binding->param1) {
@@ -55,7 +82,12 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 static int zmk_behavior_mouse_setting_init(const struct device *dev) { return 0; };
 
 static const struct behavior_driver_api zmk_behavior_mouse_setting_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed, .binding_released = on_keymap_binding_released};
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .parameter_metadata = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+};
 
 BEHAVIOR_DT_INST_DEFINE(0, zmk_behavior_mouse_setting_init, NULL, NULL, NULL, POST_KERNEL,
                         CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
